55e75c0f7595eaa4d5d2e3c0c6d062d55531f59e
pricele2 <lauren.3.price@gmail.com>
2026-06-12 13:27:42 -0500

    Working through SDP fake data

--- 01_import_descriptives.R
+++ 01_import_descriptives.R
@@ -1,25 +1,21 @@
 # # # # #
 # LPRICE5
-#
+# Demonstration project for Manager, Reporting & Analytics
 # # # # #
 
 # Define packages to load, then bring in using Librarian ----
-
-# install.packages("librarian") # NB for DPS crew: Run this the first time
-
 library(librarian)
-stock(tibble, dplyr, tidyr, readr, stringr, janitor, readxl, lubridate, openxlsx, ggplot2, googlesheets4,   # lib = lib_paths()[1L], # use to force-fix dir if necessary
-      # update_all =  TRUE, # run update_all the very first time
+stock(tibble, dplyr, tidyr, readr, stringr, janitor, readxl, lubridate, openxlsx, ggplot2, googlesheets4, forcats,
+      # update_all =  TRUE, # run update_all the first time or w new packages
       ask = TRUE)
 
-librarian::shelf(tibble, dplyr, tidyr, readr, stringr, janitor, readxl, lubridate, openxlsx, ggplot2, googlesheets4, tictoc, update_all =  FALSE, ask = TRUE)
+librarian::shelf(tibble, dplyr, tidyr, readr, stringr, janitor, readxl, lubridate, openxlsx, ggplot2, googlesheets4, tictoc, forcats,
+    update_all =  FALSE, ask = TRUE)
 
 print(.Last.value) # Confirm everything loaded appropriately
 
 # Define local WD for exports ----
-
 temp_wd = "C:/Users/Lauren/Documents/R/dps-demo"
-
 # Read in flat files from Google Sheets ----
 # NB: Requires initial authorization in browser (after: choose option 2)
 
@@ -42,25 +38,7 @@ orig_df = read_sheet(
 Sys.sleep(1) # pause for google api if retrying
 toc() ## this takes crazy long! export to local for future utility
 
-## School names crosswalk ----
-tic()
-sch_names = read_sheet(
-  "https://docs.google.com/spreadsheets/d/1TYbfKqOkfkUJajM-_0b2OYroUIKSBuFhfxuS0-ptf3s/edit?usp=sharing",
-  range = "sch-name-id!A6:K26",
-  col_names = TRUE,
-  col_types = NULL,
-  na = "",
-  trim_ws = TRUE,
-  .name_repair = "unique") |>
-  as_tibble() |>
-  clean_names() |>
-  select(-c(starts_with("district"), ends_with("grade"), setting, school_year))
-
-Sys.sleep(1)
-toc()
-
-
-## Export files the first time ----
+### Export stu-level data the first time ----
 write_excel_csv(orig_df,
     paste0(temp_wd, "/", today(), "_orig_df.csv"),
     na = "",
@@ -69,12 +47,91 @@ write_excel_csv(orig_df,
     quote = "needed",
     escape = "none",  eol = "\r\n")
 
+## Read in flat file of student data from local ----
+orig_local <- read_csv(paste0(temp_wd, "/", today(),"_orig_df.csv")) |>
+
+## School names crosswalk ----
+tic()
+sch_names = read_sheet(
+  "https://docs.google.com/spreadsheets/d/1TYbfKqOkfkUJajM-_0b2OYroUIKSBuFhfxuS0-ptf3s/edit?usp=sharing",
+  range = "sch-name-id!A6:L26",
+  col_names = TRUE,
+  col_types = NULL,
+  na = "",
+  trim_ws = TRUE,
+  .name_repair = "unique") |>
+  as_tibble() |>
+  clean_names() |>
+  select(-c(starts_with("district"), starts_with("cde_"), setting, school_year))
+
+Sys.sleep(1)
+toc() # fine but make a local backup in case of connectivity
+
+### Export crosswalk the first time ----
+# write_excel_csv(sch_names,
+#   paste0(temp_wd, "/", today(), "_sch_crosswalk.csv"),
+#   na = "",
+#   append = FALSE,
+#   col_names = TRUE,
+#   quote = "needed",
+#   escape = "none",  eol = "\r\n")
+
+# Exploratory on descriptives & missings ----
+
+## School mobility prevalence ----
+temp0 = orig_local |>
+  group_by(stu_id) |>
+  summarise(unq_schs = n_distinct(sch_id)) |>
+  ungroup()
+max(temp0$unq_schs)
+
+## Panel data for same kids - confirm? ----
+length(unique(orig_local$stu_id))
+# 18791
+temp1 = orig_local |>
+  group_by(stu_id) |>
+  summarise(unq_years = n_distinct(year)) |>
+  ungroup()
+max(temp1$unq_years) # how TF do none of the kids repeat across years?
+
+## kiddos testing in spanish - trends? ----
+table(orig_local$test, orig_local$subject, orig_local$year)
+table(orig_local$test, orig_local$subject, orig_local$grade)
+
+## WIDA is a EL proficiency test ----
+# parallel to TELPAS, probably? Who is taking this WIDA instrument?
+table(orig_local$access, orig_local$test, orig_local$grade, exclude = NULL)
+# oooh yikes for grades 3 and 4
+# ... do the later years show continued monitoring ? check if it aligns with EL flag
+temp2 = orig_local |>
+  filter(test == "CMAS SLA") |>
+  select(sch_id, grade, ell, access)
+
+table(temp2$ell, temp2$access, exclude = NULL) # ok, not perfectly "ell" kids
+
+temp3 = orig_local |>
+  filter(!is.na(access)) |>
+  select(sch_id, grade, ell, test, year)
+
+table(temp3$ell, temp3$test, temp3$grade, exclude = NULL) # there are 77 kids *not* flagged as ELL who sat the WIDA
+
+table(temp3$ell, temp3$test, temp3$year, exclude = NULL)
+
+# No, doesn't align with the ELL flag in grades 3 and 4
+temp4 = orig_local |> filter(ell == "Y", grade <= 4)
+
+# How many kids?
+length(unique(temp4$stu_id))
+
+# Every year? yes
+table(temp4$access, temp4$test, temp4$grade, temp4$year, exclude = NULL)
 
 
-# Read in flat file from local ----
-orig_local <- read.csv(paste0(temp_wd, "/", today(),"_orig_df.csv"), stringsAsFactors=TRUE)
+## Clean up from exploratory----
+rm(list = ls(pattern = "^temp"))
 
-## Recodes! for multilevel modeling ----
+
+# Recodes / forcats! for multilevel modeling ----
 
 ### sex - F ----
 # NB: three factor levels per C.R.S. 25-2- 113.8
@@ -108,23 +165,7 @@ orig_local <- read.csv(paste0(temp_wd, "/", today(),"_orig_df.csv"), stringsAsFa
 ### baseline year, and add label (forcats) ----
 
 
+## Add school ID names from crosswalk ----
 
 
 
-## Codebook out the vardefs, descriptives & missings ----
-table(orig_df$sch_id, orig_df$grade, orig_df$year) # yup, got some K8s here
-
-# length(unique(orig_df$stu_id))
-# 18791
-
-# ACCESS_Overall is the student’s WIDA ACCESS level.
-
-
-
-
-
-# Refs & Notes for later in the project ----
-
-# https://drbtlr.github.io/beating-the-odds/beating-the-odds-code.html
-
-# source("https://raw.githubusercontent.com/UrbanInstitute/urban_R_theme/master/urban_theme_windows.R")
--- README.md
+++ README.md
@@ -1,8 +1,7 @@
-## Demo project
-
+# Demo project
 repo title: ubiquitous-spork
 
-### Task directions
+## Task directions
 
 Create a visual presentation of no more than 10 slides with your analysis.
 
@@ -12,29 +11,51 @@ Uses Google Slides, and incorporates visualizations and interpretations.
 
 You will have approximately 15 minutes to discuss your performance task – 10 minutes to walk through your presentation and 5 minutes to share your data approach.
 
-1.  Title page & intro
+1.  Title page & intro & photo of presenter
 
 2.  Task at hand, overview: "Provide an overview of performance results, trends, and recommendations that district leaders should know about CMAS student performance"
 
+- Anybody can give you a district-wide frequency table by grade and subject, which I have done at [private url]
+
+- My Goal: Predict school performance, and compare these predictions with actual school performance. Are the coefficients relatively stable in each year? 
+
+- Which types of schools are over- or under-performing in math or ELA, considering the non-school factors captured in the student demographic characteristics? 
+
+- Drawing heavily on the BTO Analysis project (2020) from the Strategic Data Project at Harvard GSE, with Appalachia REL and Kentucky DOE. Also https://libguides.princeton.edu/R-Panel 
+
 3.  20 schools and 4 acad years, n unique students
-- multiple student-level vars 
-- curiously no metrics for FRL/Eco-Dis
+- 
+- Student-level categorical vars: Grade (6), Sex (3), 
+- Curiously
+- No metrics for FRL/Eco-Dis, or attendance, or which form of test (alt?!)
+- Atypically for a school dataset, no students appearing in more than 1 year
 
-4.  
+4.  District POV: Math
 
-5.  
+5.  District POV: ELA
 
-6.  
+6.  District POV: ELL & WIDA Scores
+- EL Proficiency at 4.5 (rounds to 5)
 
-7.  
+7.  GT and SPED
 
 8.  
 
-9.  
+9.
 
-10. More details at pricele2.github.io
+10. If this was not a demo project: 
 
-11. Appendix, baybeeeee
+- Campus Report Card with grade levels and special pops, with Quarto and a for-loop 
+- Either PDF or HTML 
+- Make it interactive! Compare two schools on any 2 metrics
+**- See my mockup built in Lucid at URL**
+- Talked to university and industry colleagues about the model assumptions! 
+- Identify clusters of actionable elements across schools
 
+11. More details at pricele2.github.io / Appendix, baybeeeee
 
+- Citations
+- `lmer4` and author workshop at https://pages.stat.wisc.edu/~bates/UseR2008/WorkshopD.pdf 
+- `PLM` and suggested tests at https://libguides.princeton.edu/R-Panel 
+- `merTools` (mixed-effects representation)
 
